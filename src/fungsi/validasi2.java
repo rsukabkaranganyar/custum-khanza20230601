40238fd82149985c13f77d7749f68c1ca2c4c6ae
Christopher Nanda Jonathan <cnjonathan.cnj@gmail.com>
2023-08-16 08:26:53 +0700

    tambah fungsi validasi2

--- src/fungsi/validasi2.java
+++ src/fungsi/validasi2.java
@@ -51,11 +51,24 @@ import widget.ComboBox;
 import widget.Tanggal;
 import widget.TextArea;
 import java.io.File;
+import javax.print.PrintService;
+import javax.print.PrintServiceLookup;
+import javax.print.attribute.HashPrintRequestAttributeSet;
+import javax.print.attribute.HashPrintServiceAttributeSet;
+import javax.print.attribute.PrintRequestAttributeSet;
+import javax.print.attribute.PrintServiceAttributeSet;
+import javax.print.attribute.standard.Copies;
+import javax.print.attribute.standard.MediaSizeName;
+import javax.print.attribute.standard.OrientationRequested;
+import javax.print.attribute.standard.PrinterName;
+import net.sf.jasperreports.engine.export.JRPrintServiceExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimplePrintServiceExporterConfiguration;
 /**
  *
  * @author Owner
  */
-public final class validasi {
+public final class validasi2 {
     private int a,j,i,result=0;
     private String s,s1,auto,PEMBULATANHARGAOBAT=koneksiDB.PEMBULATANHARGAOBAT();
     private final Connection connect=koneksiDB.condb();
@@ -76,7 +89,7 @@ public final class validasi {
     private String[] nomina={"","satu","dua","tiga","empat","lima","enam",
                          "tujuh","delapan","sembilan","sepuluh","sebelas"};
     
-    public validasi(){
+    public validasi2(){
         super();
     };
 
@@ -750,13 +763,68 @@ public final class validasi {
                 
                 JasperPrint jasperPrint = JasperFillManager.fillReport(namafile, parameters,rsdt);
 
-                JasperViewer jasperViewer = new JasperViewer(jasperPrint, false);
-                jasperViewer.setTitle(judul);
-                Dimension screen=Toolkit.getDefaultToolkit().getScreenSize();
-                jasperViewer.setSize(screen.width-50,screen.height-50);
-                jasperViewer.setModalExclusionType(ModalExclusionType.TOOLKIT_EXCLUDE);
-                jasperViewer.setLocationRelativeTo(null);
-                jasperViewer.setVisible(true);
+//                JasperViewer jasperViewer = new JasperViewer(jasperPrint, false);
+//                jasperViewer.setTitle(judul);
+//                Dimension screen=Toolkit.getDefaultToolkit().getScreenSize();
+//                jasperViewer.setSize(screen.width-50,screen.height-50);
+//                jasperViewer.setModalExclusionType(ModalExclusionType.TOOLKIT_EXCLUDE);
+//                jasperViewer.setLocationRelativeTo(null);
+//                jasperViewer.setVisible(true);
+
+                //Get the printers names
+                PrintService[] services = PrintServiceLookup.lookupPrintServices(null, null);
+
+                //Lets set the printer name based on the registered printers driver name (you can see the printer names in the services variable at debugging) 
+                String selectedPrinter = "Toper Printer";   
+                 //String selectedPrinter = "\\\\10.77.12.225\\Toper Printer"; // examlpe to network shared printer
+
+                System.out.println("Number of print services: " + services.length);
+                PrintService selectedService = null;
+
+                //Set the printing settings
+                PrintRequestAttributeSet printRequestAttributeSet = new HashPrintRequestAttributeSet();
+                printRequestAttributeSet.add(MediaSizeName.ISO_A4);
+                printRequestAttributeSet.add(new Copies(1));
+                if (jasperPrint.getOrientationValue() == net.sf.jasperreports.engine.type.OrientationEnum.LANDSCAPE) { 
+                  printRequestAttributeSet.add(OrientationRequested.LANDSCAPE); 
+                } else { 
+                  printRequestAttributeSet.add(OrientationRequested.PORTRAIT); 
+                } 
+                PrintServiceAttributeSet printServiceAttributeSet = new HashPrintServiceAttributeSet();
+                printServiceAttributeSet.add(new PrinterName(selectedPrinter, null));
+
+                JRPrintServiceExporter exporter = new JRPrintServiceExporter();
+                SimplePrintServiceExporterConfiguration configuration = new SimplePrintServiceExporterConfiguration();
+                configuration.setPrintRequestAttributeSet(printRequestAttributeSet);
+                configuration.setPrintServiceAttributeSet(printServiceAttributeSet);
+                configuration.setDisplayPageDialog(false);
+                configuration.setDisplayPrintDialog(false);
+
+                exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+                exporter.setConfiguration(configuration);
+
+                //Iterate through available printer, and once matched with our <selectedPrinter>, go ahead and print!
+                if(services != null && services.length != 0){
+                  for(PrintService service : services){
+                      String existingPrinter = service.getName();
+                      if(existingPrinter.equals(selectedPrinter))
+                      {
+                          selectedService = service;
+                          break;
+                      }
+                  }
+                }
+                if(selectedService != null)
+                {   
+                  try{
+                      //Lets the printer do its magic!
+                      exporter.exportReport();
+                  }catch(Exception e){
+                System.out.println("JasperReport Error: "+e.getMessage());
+                  }
+                }else{
+                  System.out.println("JasperReport Error: Printer not found!");
+                }
             } catch (Exception rptexcpt) {
                 System.out.println("Report Can't view because : " + rptexcpt);
                 JOptionPane.showMessageDialog(null,"Report Can't view because : "+ rptexcpt);
